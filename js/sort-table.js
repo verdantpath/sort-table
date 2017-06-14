c257899daf2394b16da402af8c32d2f68c75c107
Matt Dittmann <matt@doreydesigngroup.com>
2017-06-14 13:53:04 -0700

    update js

--- js/sort-table.js
+++ js/sort-table.js
@@ -25,3 +25,35 @@ var compare = {
     return a - b;
   }
 };
+
+$('.sortable').each(function() {
+  var $table = $(this);
+  var $tbody = $table.find('tbody');
+  var $controls = $table.find('th');
+  var rows = $tbody.find('tr').toArray();
+
+  $controls.on('click', function() {
+    var $header = $(this);
+    var order = $header.data('sort');
+    var column;
+
+    if ($header.is('.ascending') || $header.is('.descending')) {
+      $header.toggleClass('acsending descending');
+      $tbody.append(rows.reverse();
+    } else {
+      $header.addClass('ascending');
+      $header.siblings().removeClass('ascending descending');
+      if (compare.hasOwnProperty(order)) {
+        column = $controls.index(this);
+
+        rows.sort(function(a, b){
+          a = $(a).find('td').eq(column).text();
+          b = $(b).find('td').eq(column).text();
+          return compare[order](a, b);
+        });
+
+        $tbody.append(rows);
+      }
+    }
+  });
+});
